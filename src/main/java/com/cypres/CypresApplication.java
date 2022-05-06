4f17fddd2edfd4fee67bb15af378a4e7748fc265
eveloso-src <emiliano.veloso@gmail.com>
2022-05-06 13:54:35 -0300

    initial

---
+++ src/main/java/com/cypres/CypresApplication.java
@@ -0,0 +1,13 @@
+package com.cypres;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CypresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CypresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cypres/CypresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cypres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CypresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
